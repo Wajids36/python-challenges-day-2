b50c37df5819353a80f9d6b907a1b7f7aefc0b48
Wajids36 <161895021+Wajids36@users.noreply.github.com>
2025-02-25 17:44:33 +0500

    Add files via upload

---
+++ challenge2.py
@@ -0,0 +1,11 @@
+# Day 02 - Daily Python Challenge 
+
+# Challenge: Aisa python program likhna he jo user se ek sentence le or usme jitne words hain count kare!
+
+sentence = input("Python is amazing")
+
+words = sentence.split()
+
+word_count=len(words)
+
+print(f"Count The Total words{word_count}")
